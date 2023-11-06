59df0cc5650325a6c26adae2ddc7d7b6967c5b83
Virtual-forge <Lakehaylihamza@gmail.com>
2023-11-06 21:55:44 +0100

    sanity setup

---
+++ test/blog-test/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className='text-center text-xl font-bold'>This the blogger</h1>
+    </div>
+   
+  )
+}
